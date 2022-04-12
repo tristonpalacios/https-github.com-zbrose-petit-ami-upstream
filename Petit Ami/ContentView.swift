885e6f30a4a3e20eadfd80c39bc8bfee7ad743d4
tristonpalacios <triston.j.palacios@gmail.com>
2022-04-12 09:45:47 -0700

    Initial Commit

---
+++ Petit Ami/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Petit Ami
+//
+//  Created by Triston Palacios on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Petit Ami/Petit_AmiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Petit_AmiApp.swift
+//  Petit Ami
+//
+//  Created by Triston Palacios on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Petit_AmiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
